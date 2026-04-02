9fb1997fadffa317bfa4026bc44c0bf976f26b8c
MidFreai <midfreai@gmail.com>
2026-04-02 03:03:17 -0300

    adding raylib

--- nob.c
+++ nob.c
@@ -4,6 +4,7 @@
 
 #define BUILD_FOLDER "build/"
 #define MUJS_FOLDER "./mujs/"
+#define RAYLIB_FOLDER "./raylib-5.5_linux_amd64/"
 #define SRC_FOLDER "./src/"
 
 int main(int argc, char **argv){
@@ -13,9 +14,11 @@ int main(int argc, char **argv){
   if (!nob_mkdir_if_not_exists(BUILD_FOLDER)) return 1;
 
   nob_cmd_append(&cmd, "cc");
-  nob_cmd_append(&cmd, "-I"MUJS_FOLDER);
+  nob_cmd_append(&cmd, "-I"MUJS_FOLDER, "-I"RAYLIB_FOLDER"include/");
   nob_cmd_append(&cmd, "-o", BUILD_FOLDER"main");
   nob_cmd_append(&cmd, MUJS_FOLDER"one.c", SRC_FOLDER"jsbindings.c", SRC_FOLDER"main.c");
+  nob_cmd_append(&cmd, "-L"RAYLIB_FOLDER"lib/");
+  nob_cmd_append(&cmd, "-l:libraylib.a");
   nob_cmd_append(&cmd, "-lm");
   if (!nob_cmd_run(&cmd)) return 1;
 
--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
 #include <mujs.h>
 #include <stdio.h>
 #include <mujs.h>
+#include <raylib.h>
+
 #define JSB_IMPLEMENTATION
 #include "jsbindings.c"
 
@@ -8,6 +10,17 @@ int main(int argc, char **argv){
   js_State *J = js_newstate(NULL, NULL, 0);
   jsB_initbindings(J);
   js_dofile(J, argv[1]);
+
+  InitWindow(800, 450, "raylib [core] example - basic window");
+
+  while (!WindowShouldClose()){
+      BeginDrawing();
+      ClearBackground(RAYWHITE);
+      DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+      EndDrawing();
+  }
+
+  CloseWindow();
 
   js_gc(J, 0);
 	js_freestate(J);
